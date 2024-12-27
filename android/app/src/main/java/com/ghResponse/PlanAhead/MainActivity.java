74378f8ffd8f5082a7a40c45a22253c835ca9e3a
simon_lepine <simon@lakebed.io>
2024-12-27 15:48:37 +0000

    sync

--- android/app/src/main/java/com/ghResponse/PlanAhead/MainActivity.java
+++ android/app/src/main/java/com/ghResponse/PlanAhead/MainActivity.java
@@ -1,16 +1,5 @@
 package com.ghResponse.screensaver;
-import com.lianlf.capacitoryfullscreen.ToggleFullScreen;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-@Override
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-        add(ToggleFullScreen.class);
-    }});
-}
-}
+public class MainActivity extends BridgeActivity {}
