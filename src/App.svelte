526b3217c5e572698b5906b61b34c568368565d7
simon_lepine <simon@lakebed.io>
2024-12-27 14:52:40 +0000

    sync

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
 import { KeepAwake } from '@capacitor-community/keep-awake';
+const keepAwake = async () => {
+	await KeepAwake.keepAwake();
+};
 
 const screen_height = screen.height;
 const screen_width = screen.width;
